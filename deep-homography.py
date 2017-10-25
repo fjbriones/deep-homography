91fa1a4099e6e22bbfaccacf020ca7fc244b1b5f
fjbriones <fjbriones@outlook.com>
2017-10-25 10:33:06 +0800

    Updated the calculation of error

--- deep-homography.py
+++ deep-homography.py
@@ -3,8 +3,6 @@ from keras.models import Sequential, load_model
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Reshape, Activation, Lambda
 from keras.optimizers import SGD
 from keras import backend as K
-#from scipy.misc import imresize
-from PIL import Image
 import glob
 import numpy as np
 import os.path
@@ -116,9 +114,13 @@ def test_model(model_save_path, test_data_path, test_size=5000, batch_size=64):
         i = i + 1
         if i % batch_size == 0:
             offsets_predicted = model_l.predict(images)*64
-            j = j + 1
-            error[j] = np.average(np.abs(offsets - offsets_predicted))
+            x_1 = np.sqrt((offsets[:, 0] - offsets_predicted[:, 0])**2 + (offsets[:, 1] - offsets_predicted[:, 1])**2)
+            x_2 = np.sqrt((offsets[:, 2] - offsets_predicted[:, 2])**2 + (offsets[:, 3] - offsets_predicted[:, 3])**2)
+            x_3 = np.sqrt((offsets[:, 4] - offsets_predicted[:, 4])**2 + (offsets[:, 5] - offsets_predicted[:, 5])**2)
+            x_4 = np.sqrt((offsets[:, 6] - offsets_predicted[:, 6])**2 + (offsets[:, 7] - offsets_predicted[:, 7])**2)
+            error[j] = np.average(x_1 + x_2 + x_3 + x_4)/4
             print('Mean Corner Error: ', error[j])
+            j = j + 1
             i = 0
     print('Mean Average Corner Error: ', np.average(error))
 
